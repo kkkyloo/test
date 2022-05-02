e5405121c266652f6d7a86d6462e4c35c7c9c2af
kkkyloo <92991764+kkkyloo@users.noreply.github.com>
2022-05-02 23:20:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+  $('.kundastemen').click(function (e) {
+    e.preventDefault();
+    $('.povekunos').addClass('discovered');
+    });
+  $('.closepovekunos').click(function (e) {
+    e.preventDefault();
+    $('.povekunos').removeClass('discovered');
+    });
+
